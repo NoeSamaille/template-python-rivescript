599e191b25325d0c1eb990944e54f8f6836d3138
Noé Samaille <noe.samaille@ibm.com>
2022-12-19 11:34:39 +0000

    Adds discord integration
    Signed-off-by: Noé Samaille <noe.samaille@ibm.com>

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Template Python Rivescript
 
 Python starter kit for building chatbot with Rivescript.
+
+## Requirements
+
+1. Create a [Discord application](https://discord.com/developers/applications).
+2. Add a bot to your application by clicking **Add Bot** in **Bot** section.
+3. Save the Bot token and add it to the `DISCORD_TOKEN` environment variable of your terminal before running `script.py`.
+4. Generate an invite URL for your bot by going to **URL Generator** in **OAuth2** section, then give your bot the required permissions:
+  1. Select `bot` scope.
+  2. Select `Read Messages/View Channels` in **GENERAL PERMISSIONS**.
+  3. Select `Send Messages`, `Embed Links`, `Attach Files` in **TEXT PERMISSIONS**.
+  4. Optionnally, add any other permissions that you want.
+5. Copy the generated URL and open it to invite your bot to the required Discord server.
+
+```sh
+export DISCORD_TOKEN="<DISCORD_BOT_TOKEN>"
+python script.py
+```
--- script.py
+++ script.py
@@ -1,13 +1,32 @@
 from rivescript import RiveScript
+import discord
+import os
 
+# Inits Rivescript bot
 bot = RiveScript()
 bot.load_directory("./eg/brain")
 bot.sort_replies()
 
-while True:
-    msg = input('You> ')
-    if msg == '/quit':
-        quit()
+# Inits Discord client
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
 
-    reply = bot.reply("localuser", msg)
-    print('Bot>', reply)
+# Handles Discord login event
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+# Handles Discord message event
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$bot '):
+        msg = message.content.replace('$bot ', '')
+        reply = bot.reply("localuser", msg)
+        await message.channel.send(reply)
+
+# Run discord client using bot token
+client.run(os.environ['DISCORD_TOKEN'])
